7617e2f59bad274b73ed71c70dfe9a3338ca25d6
Niyathi arun <149257438+niyathiarun@users.noreply.github.com>
2026-02-08 22:51:12 +0530

    Reorder and update function declarations in stats.h

--- stats.h
+++ stats.h
@@ -1,57 +1,12 @@
 #ifndef STATS_H
 #define STATS_H
 
-/**
- * @brief Sorts array from largest to smallest
- * @param ptr Pointer to unsigned char array
- * @param length Size of array
- */
-void sort_array(unsigned char *ptr, unsigned int length);
-
-/**
- * @brief Returns median value (sorts array first)
- * @param ptr Pointer to unsigned char array  
- * @param length Size of array
- * @return Median value
- */
 unsigned char find_median(unsigned char *ptr, unsigned int length);
-
-/**
- * @brief Returns mean value (integer division)
- * @param ptr Pointer to unsigned char array
- * @param length Size of array
- * @return Mean value floored
- */
 unsigned char find_mean(unsigned char *ptr, unsigned int length);
-
-/**
- * @brief Returns maximum value
- * @param ptr Pointer to unsigned char array
- * @param length Size of array
- * @return Maximum value
- */
 unsigned char find_maximum(unsigned char *ptr, unsigned int length);
-
-/**
- * @brief Returns minimum value  
- * @param ptr Pointer to unsigned char array
- * @param length Size of array
- * @return Minimum value
- */
 unsigned char find_minimum(unsigned char *ptr, unsigned int length);
-
-/**
- * @brief Prints array to screen
- * @param ptr Pointer to unsigned char array
- * @param length Size of array
- */
+void sort_array(unsigned char *ptr, unsigned int length);
 void print_array(unsigned char *ptr, unsigned int length);
-
-/**
- * @brief Prints all statistics
- * @param ptr Pointer to sorted unsigned char array
- * @param length Size of array
- */
 void print_statistics(unsigned char *ptr, unsigned int length);
 
-#endif /* STATS_H */
+#endif
